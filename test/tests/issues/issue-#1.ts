5bb3f079ae9112ad99b99db3f017961991f4e04b
Kévin LABESSE <kevin.labesse@neo9.fr>
2019-02-17 12:24:52 +0100

    issue1: Add test to check recursively properties removal ISSUE-1

--- test/tests/issues/issue-#1.ts
+++ test/tests/issues/issue-#1.ts
@@ -10,8 +10,12 @@ class SampleTypeListing extends BaseMongoObject {
 
 class SampleType extends SampleTypeListing {
 	public code: string;
-	public field1String: string;
-	public field2String: string;
+	public field1: string;
+	public field2: string;
+	public sub?: {
+		field3: string;
+		field4?: string;
+	};
 }
 
 global.log = new N9Log('tests').module('issues');
@@ -34,27 +38,67 @@ test('[ISSUE#1] updateManyAtOnce should remove properties if not specified', asy
 
 	const savedObject = await mongoClient.insertOne({
 		code: 'issue#1',
-		field1String: 'string1',
-		field2String: 'string2'
+		field1: 'string1',
+		field2: 'string2'
 	}, 'userId1');
 
 	const foundObject = await mongoClient.findOneById(savedObject._id);
 
 	t.truthy(foundObject, 'found by query');
-	t.is(foundObject.field1String, "string1", 'found right element string1');
-	t.is(foundObject.field2String, "string2", 'found right element string2');
+	t.is(foundObject.field1, "string1", 'found right element string1');
+	t.is(foundObject.field2, "string2", 'found right element string2');
 
 	global.log.debug(`updateManyAtOnce with savedObject without field1String field`);
-	const updateArray = await (await mongoClient.updateManyAtOnce([{ code: 'issue#1', field2String: 'string42' }], 'userId1', true, false, 'code')).toArray();
+	const updateArray = await (await mongoClient.updateManyAtOnce([{ code: 'issue#1', field2: 'string42' }], 'userId1', true, false, 'code')).toArray();
 
 	const updatedObject = updateArray[0];
 
 	global.log.debug(`Updated object : `, updatedObject);
 
-	t.is(updatedObject.field2String, "string42", 'after update - found right element string42');
-	t.falsy(updatedObject.field1String, 'after update - field1String should not exists anymore');
+	t.is(updatedObject.field2, "string42", 'after update - found right element string42');
+	t.falsy(updatedObject.field1, 'after update - field1 should not exists anymore');
 	t.truthy(updatedObject._id, 'after update - _id should still exists');
 	t.truthy(updatedObject.objectInfos, 'after update - objectInfos should still exists');
+
+	await mongoClient.dropCollection();
+});
+
+test('[ISSUE#1] updateManyAtOnce should remove recursively properties if not specified', async (t: Assertions) => {
+	const mongoClient = new MongoClient('test-' + Date.now(), SampleType, SampleTypeListing);
+	const size = await mongoClient.count();
+
+	t.true(size === 0);
+
+	const savedObject = await mongoClient.insertOne({
+		code: 'issue#1',
+		field1: 'string1',
+		field2: 'string2',
+		sub: {
+			field3: 'string3',
+			field4: 'string4'
+		}
+	}, 'userId1');
+
+	const foundObject = await mongoClient.findOneById(savedObject._id);
+
+	t.truthy(foundObject, 'found by query');
+	t.is(foundObject.field1, "string1", 'found right element string1');
+	t.is(foundObject.field2, "string2", 'found right element string2');
+
+	global.log.debug(`updateManyAtOnce with savedObject without field1String field`);
+	const updateArray = await (await mongoClient.updateManyAtOnce([{ code: 'issue#1', field2: 'string42', sub: { field3: 'string43' } }], 'userId1', true, false, 'code')).toArray();
+
+	const updatedObject = updateArray[0];
+
+	global.log.debug(`Updated object : `, updatedObject);
+
+	t.is(updatedObject.field2, "string42", 'after update - found right element string42');
+	t.falsy(updatedObject.field1, 'after update - field1 should not exists anymore');
+	t.truthy(updatedObject._id, 'after update - _id should still exists');
+	t.truthy(updatedObject.objectInfos, 'after update - objectInfos should still exists');
+
+	t.is(updatedObject.sub.field3, "string43", 'after update - found right element string43');
+	t.falsy(updatedObject.sub.field4, 'after update - sub.field4 should not exists anymore');
 
 	await mongoClient.dropCollection();
 });
