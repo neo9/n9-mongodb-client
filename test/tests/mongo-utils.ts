d509d28da031555519eb71b3059fbcdd459550e7
Maxime Fradin <maxime.fradin@neo9.fr>
2019-07-24 16:32:46 +0200

    mongo: Add mongo uri with password test

--- test/tests/mongo-utils.ts
+++ test/tests/mongo-utils.ts
@@ -1,6 +1,8 @@
 import { N9Log } from '@neo9/n9-node-log';
 import test, { Assertions } from 'ava';
+import * as _ from 'lodash';
 import { ObjectID } from 'mongodb';
+import * as stdMocks from 'std-mocks';
 import { MongoUtils } from '../../src';
 
 global.log = new N9Log('tests').module('mongo-utils');
@@ -25,4 +27,28 @@ test('[MONGO-UTILS] mapObjectToClass null', async (t: Assertions) => {
 	t.deepEqual(MongoUtils.mapObjectToClass(null, undefined), undefined, 'should return undefined');
 	t.deepEqual(MongoUtils.mapObjectToClass(null, 0), 0, 'should return 0');
 	t.deepEqual(MongoUtils.mapObjectToClass(null, ''), '', 'should return ""');
+});
+
+test('[MONGO-UTILS] URI connection log', async (t: Assertions) => {
+	const mongoURI = 'mongodb://localhost:27017/test-n9-mongo-client';
+	const mongoURIregex = new RegExp(_.escapeRegExp(mongoURI));
+
+	stdMocks.use();
+	await MongoUtils.connect(mongoURI);
+	await MongoUtils.disconnect();
+	let output = stdMocks.flush();
+	stdMocks.restore();
+
+	t.regex(output.stdout[0], mongoURIregex, 'URI should be identic');
+
+	const mongoPassword = 'PaSsw0rD';
+	const mongoURIWithPassword = `mongodb://login:${mongoPassword}@localhost:27017/test-n9-mongo-client`;
+	const mongoURIPasswordRegex = new RegExp(_.escapeRegExp(mongoPassword));
+
+	stdMocks.use();
+	await t.throwsAsync(MongoUtils.connect(mongoURIWithPassword));
+	output = stdMocks.flush();
+	stdMocks.restore();
+
+	t.notRegex(output.stdout[0], mongoURIPasswordRegex, 'Password should not be displayed in URI');
 });
