54b617da824e23960bf7c8344ca851654f4e99ff
Cédric Ribeiro <cedric.ribeiro@neo9.fr>
2019-07-29 11:19:18 +0200

    find: Add collations parameters

---
+++ test/tests/collation.ts
@@ -0,0 +1,86 @@
+import { N9Log } from '@neo9/n9-node-log';
+import test, { Assertions } from 'ava';
+import { CollationDocument } from 'mongodb';
+import * as mongodb from 'mongodb';
+
+import { MongoClient, MongoUtils } from '../../src';
+import { BaseMongoObject } from '../../src/models';
+
+class SampleTypeListing extends BaseMongoObject {
+	public field1String: string;
+}
+
+class SampleType extends SampleTypeListing {
+	public field2Number: number;
+	public field3String?: string;
+}
+
+global.log = new N9Log('tests');
+
+test.before(async () => {
+	await MongoUtils.connect('mongodb://localhost:27017/test-n9-mongo-client');
+});
+
+test.after(async () => {
+	global.log.info(`DROP DB after tests OK`);
+	await (global.db as mongodb.Db).dropDatabase();
+	await MongoUtils.disconnect();
+});
+
+test('[CRUD] Insert multiples and find with collation', async (t: Assertions) => {
+	const mongoClient = new MongoClient('test-' + Date.now(), SampleType, SampleTypeListing);
+	const size = await mongoClient.count();
+
+	t.true(size === 0, 'collection should be empty');
+
+	const intValue = 41;
+	await mongoClient.insertOne({
+		field1String: 'test',
+		field2Number: intValue
+	}, 'userId1');
+
+	await mongoClient.insertOne({
+		field1String: 'Test',
+		field2Number: intValue
+	}, 'userId1');
+
+	await mongoClient.insertOne({
+		field1String: 'Têst',
+		field2Number: intValue
+	}, 'userId1');
+
+	await mongoClient.insertOne({
+		field1String: 'têst',
+		field2Number: intValue
+	}, 'userId1');
+
+	const collationStrength1: CollationDocument = { locale: 'fr', strength: 1 };
+	const collationStrength2: CollationDocument = { locale: 'fr', strength: 2 };
+
+	const sizeWithElementIn = await mongoClient.count();
+	const findWithoutCollation = await mongoClient.find({ field1String: 'test' });
+	const findAllWithoutCollation = await mongoClient.find({});
+	const findWithCollationStrengthOne = await mongoClient.find(
+		{ field1String: 'test' },
+		undefined,
+		undefined,
+		undefined,
+		undefined,
+		collationStrength1);
+
+	const findWithCollationStrengthTwo = await mongoClient.find(
+		{ field1String: 'test' },
+		undefined,
+		undefined,
+		undefined,
+		undefined,
+		collationStrength2);
+
+	t.is(sizeWithElementIn, 4, 'nb element in collection');
+	t.is(await findWithCollationStrengthOne.count(), 4, 'nb element collation strength 1');
+	t.is(await findWithCollationStrengthTwo.count(), 2, 'nb element collation strength 2');
+	t.is(await findAllWithoutCollation.count(), 4, 'nb elements');
+	t.is(await findWithoutCollation.count(), 1, 'nb element without collation');
+
+	await mongoClient.dropCollection();
+});
