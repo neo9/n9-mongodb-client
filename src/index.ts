ba274f68189ddf000396bc05c255c69cc989af8a
Clement Petit <clement.petit@neo9.fr>
2020-03-13 10:57:51 +0100

    client: Move client configuration model in separate file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './client';
 export * from './lock';
+export * from './models';
 export * from './mongo-read-stream';
 export * from './mongo-utils';
 export * from './aggregation-utils';
