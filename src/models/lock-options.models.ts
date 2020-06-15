088d218a103370ba02c9b3a781975bbee5ca418c
Benjamin Daniel <benjamin.daniel@neo9.fr>
2020-06-15 19:22:48 +0200

    locks: Set lock options optional

--- src/models/lock-options.models.ts
+++ src/models/lock-options.models.ts
@@ -3,15 +3,15 @@ import * as mongoDbLock from 'mongodb-lock';
 export class LockOptions implements mongoDbLock.LockOptions {
 	public timeout?: number;
 	public removeExpired?: boolean;
-	public n9MongoLockOptions: {
+	public n9MongoLockOptions?: {
 		/**
 		 * Check timeout randomly computed between waitDurationMsMin and waitDurationMsMax ms
 		 * to acquire the lock
 		 */
-		waitDurationMsMax: number;
+		waitDurationMsMax?: number;
 		/**
 		 * Minimum wait duration between two checks to acquire the lock
 		 */
-		waitDurationMsMin: number;
+		waitDurationMsMin?: number;
 	};
 }
