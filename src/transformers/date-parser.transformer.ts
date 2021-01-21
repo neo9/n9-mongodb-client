d0dce0aa8edc7d133320873a37a20190637a625d
Benjamin DANIEL <benjamin.daniel@neo9.fr>
2021-01-21 18:12:56 +0100

    transformer: Fix date transformer signature

--- src/transformers/date-parser.transformer.ts
+++ src/transformers/date-parser.transformer.ts
@@ -1,11 +1,8 @@
-import { TransformFnParams } from 'class-transformer';
-
 export function toDate(value: string): Date {
 	return new Date(value);
 }
 
-export function transform(transformFnParams: TransformFnParams): Date | string {
-	const value: Date | string = transformFnParams.value;
+export function transform(value: Date | string): Date | string {
 	if (typeof value === 'string') return toDate(value);
 	return value;
 }
