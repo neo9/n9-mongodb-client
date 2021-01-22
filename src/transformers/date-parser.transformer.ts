1398234de8889d1663ad681bc2386ecfb2e5ee23
Benjamin DANIEL <benjamin.daniel@neo9.fr>
2021-01-22 09:25:54 +0100

    yarn: Move class-transformer to peer dependencies

--- src/transformers/date-parser.transformer.ts
+++ src/transformers/date-parser.transformer.ts
@@ -1,8 +1,11 @@
+import { TransformFnParams } from 'class-transformer';
+
 export function toDate(value: string): Date {
 	return new Date(value);
 }
 
-export function transform(value: Date | string): Date | string {
+export function transform(transformFnParams: TransformFnParams): Date | string {
+	const value: Date | string = transformFnParams.value;
 	if (typeof value === 'string') return toDate(value);
 	return value;
 }
