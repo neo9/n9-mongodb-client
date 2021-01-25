b1a576f0398b4be7e637e25050a29b618ab5a8d8
Benjamin DANIEL <benjamin.daniel@neo9.fr>
2021-01-25 10:59:34 +0100

    update: Expose updateMany function for advanced usages

--- src/models/update-many-query.models.ts
+++ src/models/update-many-query.models.ts
@@ -1,12 +1,32 @@
 import { FilterQuery, UpdateQuery } from 'mongodb';
 import { StringMap } from './';
 
+/**
+ * Queries to run bulk of update one
+ *
+ * Order of target fields priority : id, key, query
+ *
+ * If none provided the target is `_id does not exist`
+ */
 export class UpdateManyQuery<T> {
+	/**
+	 * If provided the _id of the target
+	 */
 	public id?: string;
+	/**
+	 * If provided the name and value of the target pointed with one field only
+	 */
 	public key?: {
 		name: string;
 		value: string | number | boolean;
 	};
+	/**
+	 * If provided the target of the entity to update
+	 */
 	public query?: FilterQuery<T>;
+
+	/**
+	 * The update to run, will be filled with objectInfos update
+	 */
 	public updateQuery: UpdateQuery<T>;
 }
