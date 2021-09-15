ad896c80c148c065ad7d2d1179fee8abf2a52327
Théo Loïs <theo.lois@neo9.fr>
2021-09-15 11:27:20 +0200

    lock: Remove map and replace external module mongobd-lock CATW-1246

--- src/lock.ts
+++ src/lock.ts
@@ -1,19 +1,17 @@
 import { N9Error, waitFor } from '@neo9/n9-node-utils';
 import * as _ from 'lodash';
 import * as mongodb from 'mongodb';
-import * as mongoDbLock from 'mongodb-lock';
-import { StringMap } from '.';
 import { LockOptions } from './models/lock-options.models';
+import { MongodbLock } from './mongodb-lock';
 
 /**
  * Wrapper of mongodb-lock
  * https://www.npmjs.com/package/mongodb-lock
  */
 export class N9MongoLock {
-	private locks: StringMap<mongoDbLock.MongodbLock> = {};
 	private options: LockOptions;
-	private collection: string;
 	private defaultLock: string;
+	private lock: MongodbLock;
 	private readonly waitDurationMsRandomPart: number;
 
 	/**
@@ -27,7 +25,6 @@ export class N9MongoLock {
 		lockName: string = 'default-lock',
 		options?: LockOptions,
 	) {
-		this.collection = collection;
 		this.defaultLock = lockName;
 		this.options = _.defaultsDeep(options, {
 			timeout: 30 * 1000,
@@ -37,6 +34,7 @@ export class N9MongoLock {
 				waitDurationMsMin: 5,
 			},
 		});
+		this.lock = new MongodbLock(collection, this.options);
 		const db = global.db as mongodb.Db;
 		if (!db) {
 			throw new N9Error('missing-db', 500);
@@ -44,7 +42,6 @@ export class N9MongoLock {
 		this.waitDurationMsRandomPart =
 			this.options.n9MongoLockOptions.waitDurationMsMax -
 			this.options.n9MongoLockOptions.waitDurationMsMin;
-		this.locks[this.defaultLock] = mongoDbLock(db.collection(collection), lockName, options);
 	}
 
 	/**
@@ -52,34 +49,23 @@ export class N9MongoLock {
 	 * https://github.com/chilts/mongodb-lock#mongodb-indexes
 	 * @param lockName : a key to identify the lock
 	 */
-	public async ensureIndexes(lockName: string = this.defaultLock): Promise<void> {
-		const fullLockName =
-			lockName === this.defaultLock ? lockName : `${this.defaultLock}_${lockName}`;
-		return new Promise<void>((resolve, reject) => {
-			if (this.locks[fullLockName]) {
-				this.locks[fullLockName].ensureIndexes((err: any, result: any) => {
-					if (err) return reject(err);
-					return resolve(result);
-				});
-			} else {
-				return reject('Lock not found');
-			}
-		});
+	public async ensureIndexes(): Promise<void> {
+		return this.lock.ensureIndexes();
 	}
 
 	/**
 	 * Once you have a lock, you have a 30 second timeout until the lock is released. You can release it earlier by calling release
 	 * @param lockName : a key to identify the lock
 	 */
-	public async acquire(lockName: string = this.defaultLock): Promise<string | undefined> {
-		await this.createIfNotExists(lockName);
+	public async acquire(suffix?: string): Promise<string | undefined> {
 		return new Promise<string>((resolve, reject) => {
-			this.locks[
-				lockName === this.defaultLock ? lockName : `${this.defaultLock}_${lockName}`
-			].acquire((err: any, code: string) => {
-				if (err) return reject(err);
-				return resolve(code);
-			});
+			this.lock.acquireLock(
+				(err: any, code: string) => {
+					if (err) return reject(err);
+					return resolve(code);
+				},
+				suffix ? `${this.defaultLock}_${suffix}` : this.defaultLock,
+			);
 		});
 	}
 
@@ -88,13 +74,10 @@ export class N9MongoLock {
 	 * @param timeoutMs timeout in ms
 	 * @param lockName : a key to identify the lock
 	 */
-	public async acquireBlockingUntilAvailable(
-		timeoutMs: number,
-		lockName: string = this.defaultLock,
-	): Promise<string> {
+	public async acquireBlockingUntilAvailable(timeoutMs: number, suffix?: string): Promise<string> {
 		const startTime = Date.now();
 		do {
-			const code = await this.acquire(lockName);
+			const code = await this.acquire(suffix);
 			if (code) return code;
 			await waitFor(
 				this.options.n9MongoLockOptions.waitDurationMsMin +
@@ -107,50 +90,20 @@ export class N9MongoLock {
 	 * Release the lock
 	 * @param lockName : a key to identify the lock
 	 */
-	public async release(code: string, lockName: string = this.defaultLock): Promise<boolean> {
-		await this.createIfNotExists(lockName);
-		let ret: boolean = await new Promise<boolean>((resolve, reject) => {
-			this.locks[
-				lockName === this.defaultLock ? lockName : `${this.defaultLock}_${lockName}`
-			].release(code, (err: any, ok: boolean) => {
-				if (err) return reject(err);
-				return resolve(ok);
-			});
+	public async release(code: string, suffix?: string): Promise<boolean> {
+		const db = global.db as mongodb.Db;
+		const ret: boolean = await new Promise<boolean>((resolve, reject) => {
+			this.lock.releaseLock(
+				code,
+				(err: any, ok: boolean) => {
+					if (err) return reject(err);
+					return resolve(ok);
+				},
+				suffix ? `${this.defaultLock}_${suffix}` : this.defaultLock,
+			);
 		});
 		// Wait to let enough time to someone else to pick the lock
 		await waitFor(this.options.n9MongoLockOptions.waitDurationMsMax + 5);
-		if (lockName !== this.defaultLock) {
-			const obseleteCode = await this.acquire(lockName);
-			if (obseleteCode) {
-				ret = await this.deleteObseleteLock(lockName, obseleteCode);
-			}
-		}
 		return ret;
-	}
-
-	private async deleteObseleteLock(lockName: string, code: string): Promise<boolean> {
-		const ret = await new Promise<boolean>((resolve, reject) => {
-			this.locks[`${this.defaultLock}_${lockName}`].release(code, (err: any, ok: boolean) => {
-				if (err) return reject(err);
-				return resolve(ok);
-			});
-		});
-		delete this.locks[`${this.defaultLock}_${lockName}`];
-		return ret;
-	}
-
-	private async createIfNotExists(lockName: string): Promise<void> {
-		if (lockName !== this.defaultLock && !this.locks[`${this.defaultLock}_${lockName}`]) {
-			const db = global.db as mongodb.Db;
-			if (!db) {
-				throw new N9Error('missing-db', 500);
-			}
-			this.locks[`${this.defaultLock}_${lockName}`] = mongoDbLock(
-				db.collection(this.collection),
-				`${this.defaultLock}_${lockName}`,
-				this.options,
-			);
-			await this.ensureIndexes(lockName);
-		}
 	}
 }
