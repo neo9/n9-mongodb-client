69790848f49321d77ac157a8a86ff654e949ca92
Hugues Vogel <hugues.vogel@neo9.fr>
2022-10-10 09:17:21 +0200

    Client: Sometime modification date and history is not updated
    On call findOneAndUpdate with returnNewValue=false:
    - modification date is not updated
    
    On call updateMany with returnNewValue=false:
    - modification date is not updated
    - history date is not updated

--- test/update-only-on-change.test.ts
+++ test/update-only-on-change.test.ts
@@ -119,6 +119,89 @@ insertThenUpdateOneFieldToNewValue.title = (
 };
 
 /**
+ * Macro that will insert an entity then update it with findOneAndUpdate, changing one field
+ *
+ * @param t ava assertions object
+ * @param inputParams input parameters to set
+ * @param assertions assertions to perform
+ */
+async function insertThenUpdateOneFieldToNewValueWithoutReturningNewValue(
+	t: Assertions,
+	inputParams: Partial<MongoClientConfiguration>,
+	assertions: FindOneAndUpdateTestCaseAssertions,
+): Promise<void> {
+	const mongoClient = new MongoClient(`test-${Date.now()}`, SampleType, SampleType, {
+		...inputParams,
+	});
+
+	const insertedEntity = await mongoClient.insertOne({ property1: 'value1' }, 'TEST');
+	const initialLastUpdateDate = insertedEntity.objectInfos.lastUpdate.date;
+	const initialLastModificationDate = insertedEntity.objectInfos.lastModification.date;
+	t.truthy(insertedEntity.objectInfos.creation.date, 'Creation date is set upon insertion');
+	t.truthy(initialLastUpdateDate, 'Last modification date is set upon insertion');
+	t.truthy(initialLastModificationDate, 'Last modification date is set upon insertion');
+	await waitFor(10);
+
+	// check returned entity
+	await mongoClient.findOneAndUpdate(
+		{ _id: MongoUtils.oid(insertedEntity._id) as any },
+		{ $set: { property1: 'new-value1' } },
+		'TEST',
+		false,
+		false,
+		false,
+	);
+
+	// check entity in db
+	const dbEntity = await mongoClient.findOne({ _id: MongoUtils.oid(insertedEntity._id) });
+	const dbLastUpdateDate = dbEntity.objectInfos.lastUpdate.date;
+	const dbLastModificationDate = dbEntity.objectInfos.lastModification.date;
+	t.deepEqual('new-value1', dbEntity.property1, 'Property 1 did change in db');
+	t.notDeepEqual(dbLastUpdateDate, initialLastUpdateDate, 'Last update date did change in db');
+	if (assertions.lastModificationDateShouldChange) {
+		t.notDeepEqual(
+			dbLastModificationDate,
+			initialLastModificationDate,
+			'Last modification date did change in db',
+		);
+	} else {
+		t.deepEqual(
+			dbLastModificationDate,
+			initialLastModificationDate,
+			'Last modification date did not change in db',
+		);
+	}
+}
+
+insertThenUpdateOneFieldToNewValueWithoutReturningNewValue.title = (
+	providedTitle: string,
+	inputParams: Partial<MongoClientConfiguration>,
+	assertions: FindOneAndUpdateTestCaseAssertions,
+): string => {
+	const pick = inputParams.updateOnlyOnChange?.changeFilters?.pick ?? [];
+	const omit = inputParams.updateOnlyOnChange?.changeFilters?.omit ?? [];
+	const updateOnlyOnChange = inputParams.updateOnlyOnChange
+		? `enabled (pick: [${pick?.join()}], omit: [${omit?.join()}])`
+		: 'disabled';
+	const lastModificationDateShouldChange = assertions.lastModificationDateShouldChange
+		? 'last modification date did change'
+		: 'last modification date did not change';
+	let keepHistoric: string;
+	switch (inputParams.keepHistoric) {
+		case undefined:
+			keepHistoric = 'not-set';
+			break;
+		case true:
+			keepHistoric = `enabled`;
+			break;
+		case false:
+		default:
+			keepHistoric = `disabled`;
+	}
+	return `${providedTitle} findOneAndUpdate (without returning new value) when updating a field with updateOnlyOnChange ${updateOnlyOnChange} and keepHistoric ${keepHistoric} should result in ${lastModificationDateShouldChange}`;
+};
+
+/**
  * Macro that will insert an entity then update it with findOneAndUpdate, updating one field to same value
  *
  * @param t ava assertions object
@@ -241,6 +324,17 @@ ava.serial(
 
 ava.serial(
 	testPrefix,
+	insertThenUpdateOneFieldToNewValueWithoutReturningNewValue,
+	{
+		updateOnlyOnChange: {},
+	},
+	{
+		lastModificationDateShouldChange: true,
+	},
+);
+
+ava.serial(
+	testPrefix,
 	insertThenUpdateOneFieldToSameValue,
 	{
 		updateOnlyOnChange: {},
@@ -254,6 +348,17 @@ ava.serial(
 ava.serial(
 	testPrefix,
 	insertThenUpdateOneFieldToNewValue,
+	{
+		keepHistoric: true,
+	},
+	{
+		lastModificationDateShouldChange: true,
+	},
+);
+
+ava.serial(
+	testPrefix,
+	insertThenUpdateOneFieldToNewValueWithoutReturningNewValue,
 	{
 		keepHistoric: true,
 	},
@@ -291,10 +396,42 @@ for (const keepHistoric of [undefined, true, false]) {
 		},
 	);
 
+	ava.serial(
+		testPrefix,
+		insertThenUpdateOneFieldToNewValueWithoutReturningNewValue,
+		{
+			keepHistoric,
+			updateOnlyOnChange: {
+				changeFilters: {
+					pick: ['non-existant-property'],
+				},
+			},
+		},
+		{
+			lastModificationDateShouldChange: false,
+		},
+	);
+
 	// updateOnlyOnChange enabled and field picked
 	ava.serial(
 		testPrefix,
 		insertThenUpdateOneFieldToNewValue,
+		{
+			keepHistoric,
+			updateOnlyOnChange: {
+				changeFilters: {
+					pick: ['property1'],
+				},
+			},
+		},
+		{
+			lastModificationDateShouldChange: true,
+		},
+	);
+
+	ava.serial(
+		testPrefix,
+		insertThenUpdateOneFieldToNewValueWithoutReturningNewValue,
 		{
 			keepHistoric,
 			updateOnlyOnChange: {
@@ -325,10 +462,43 @@ for (const keepHistoric of [undefined, true, false]) {
 		},
 	);
 
+	ava.serial(
+		testPrefix,
+		insertThenUpdateOneFieldToNewValueWithoutReturningNewValue,
+		{
+			keepHistoric,
+			updateOnlyOnChange: {
+				changeFilters: {
+					omit: ['property1'],
+				},
+			},
+		},
+		{
+			lastModificationDateShouldChange: false,
+		},
+	);
+
 	// updateOnlyOnChange enabled and field picked and omitted
 	ava.serial(
 		testPrefix,
 		insertThenUpdateOneFieldToNewValue,
+		{
+			keepHistoric,
+			updateOnlyOnChange: {
+				changeFilters: {
+					pick: ['property1'],
+					omit: ['property1'],
+				},
+			},
+		},
+		{
+			lastModificationDateShouldChange: true, // omit is ignored
+		},
+	);
+
+	ava.serial(
+		testPrefix,
+		insertThenUpdateOneFieldToNewValueWithoutReturningNewValue,
 		{
 			keepHistoric,
 			updateOnlyOnChange: {
@@ -348,6 +518,17 @@ for (const keepHistoric of [undefined, true, false]) {
 ava.serial(
 	testPrefix,
 	insertThenUpdateOneFieldToNewValue,
+	{
+		updateOnlyOnChange: undefined,
+	},
+	{
+		lastModificationDateShouldChange: true,
+	},
+);
+
+ava.serial(
+	testPrefix,
+	insertThenUpdateOneFieldToNewValueWithoutReturningNewValue,
 	{
 		updateOnlyOnChange: undefined,
 	},
